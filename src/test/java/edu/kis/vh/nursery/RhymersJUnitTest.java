c1c9b8530acea79a2e64bf9f867c1df34b9f3e15
255701 <255701@helium.com>
2026-03-30 13:24:50 +0200

    16. testy jednostkowe dla linked list

--- src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
+++ src/test/java/edu/kis/vh/nursery/RhymersJUnitTest.java
@@ -1,4 +1,5 @@
 package edu.kis.vh.nursery;
+import edu.kis.vh.nursery.list.IntLinkedList;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -74,5 +75,47 @@ public class RhymersJUnitTest {
         result = rhymer.countOut();
         Assert.assertEquals(EMPTY_STACK_VALUE, result);
     }
+    //testy dla linked list
+    @Test
+    public void testPush() {
+        IntLinkedList list = new IntLinkedList();
+        list.push(5);
+        Assert.assertEquals(5, list.top());
+        list.push(10);
+        Assert.assertEquals(10, list.top());
+    }
 
+    @Test
+    public void testIsEmpty() {
+        IntLinkedList list = new IntLinkedList();
+        Assert.assertTrue(list.isEmpty());
+        list.push(1);
+        Assert.assertFalse(list.isEmpty());
+        list.pop();
+        Assert.assertTrue(list.isEmpty());
+    }
+
+    @Test
+    public void testTop() {
+        IntLinkedList list = new IntLinkedList();
+        final int EMPTY_STACK_VALUE = IntLinkedList.EMPTY_STACK_VALUE;
+        Assert.assertEquals(EMPTY_STACK_VALUE, list.top());
+        list.push(42);
+        Assert.assertEquals(42, list.top());
+    }
+
+    @Test
+    public void testPop() {
+        IntLinkedList list = new IntLinkedList();
+        final int EMPTY_STACK_VALUE = IntLinkedList.EMPTY_STACK_VALUE;
+        Assert.assertEquals(EMPTY_STACK_VALUE, list.pop());
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        Assert.assertEquals(3, list.pop());
+        Assert.assertEquals(2, list.pop());
+        Assert.assertEquals(1, list.pop());
+        Assert.assertEquals(EMPTY_STACK_VALUE, list.pop());
+    }
 }
