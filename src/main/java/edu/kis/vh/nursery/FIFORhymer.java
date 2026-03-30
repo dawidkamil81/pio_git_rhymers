38c8e4c0e98477ceca83281994054a9f86aedc5c
255701 <255701@helium.com>
2026-03-30 13:34:36 +0200

    17. dokumentacja dla FIFOrhymer

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,9 +1,16 @@
 package edu.kis.vh.nursery;
 
 public class FIFORhymer extends DefaultCountingOutRhymer {
-
+    /*
+        tworzy tymczasowy stos pomocniczy
+     */
     private final DefaultCountingOutRhymer temp = new DefaultCountingOutRhymer();
 
+    /*
+        zamienia LIFO w FIO czyli odwraca stos
+         przenosi elementy do tymczasowego stosu,
+         nastepnie zwraca najstarszy element.
+     */
     @Override
     public int countOut() {
         while (!callCheck())
